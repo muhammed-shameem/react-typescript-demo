414c8407c84ded81ced73f7bf6b2a2298e721731
muhammed-shameem <muhammadshami977@gmail.com>
2023-03-15 17:10:55 +0530

    basic css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes';
+import "./styles/base.css"
 
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
